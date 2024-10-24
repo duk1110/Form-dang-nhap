1955f1a5c9040d89b267a5b2c15b86324b2074a3
duk1110 <ducong2901@gmail.com>
2024-10-24 15:22:26 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font(".VnBlackH", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(354, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 44);
+            label1.TabIndex = 0;
+            label1.Text = "Log In";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(110, 157);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 38);
+            label2.TabIndex = 1;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(110, 235);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 38);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(258, 164);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(348, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(258, 242);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(348, 31);
+            textBox2.TabIndex = 4;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(258, 330);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 5;
+            button1.Text = "OK";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(494, 330);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 6;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(799, 514);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System.DirectoryServices;
+using System.Text.RegularExpressions;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;
+            string password = textBox2.Text;
+
+
+            if(email == "admin" &&  password == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công.");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại.");
+            }
+        }
+    }
+}
